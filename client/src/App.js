07b00f46f9edbfc54d941357529ef17850813dc7
yara <yaraeb121@gmail.com>
2021-08-02 00:06:26 +0200

    edit react

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Button } from "reactstrap";
+function App() {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <Button color="danger">Danger!</Button>
+    </div>
+  );
+}
+
+export default App;
